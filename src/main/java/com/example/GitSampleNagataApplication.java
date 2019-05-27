933738ff51d455845e3fe43e4298f08f6b43404b
koichi.nagata <koichi.nagata@rakus.co.jp>
2019-05-27 13:44:36 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/GitSampleNagataApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleNagataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleNagataApplication.class, args);
+	}
+
+}
