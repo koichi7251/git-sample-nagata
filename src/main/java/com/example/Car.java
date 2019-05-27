3ac6c2f2f65e7d04f2ac9f78b2090e2009995c26
koichi.nagata <koichi.nagata@rakus.co.jp>
2019-05-27 13:48:07 +0900

    車に関するクラスを作成しました。

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Car {
+	private int speed;
+	private String bodyColor;
+	
+	public final int getSpeed() {
+		return speed;
+	}
+	public final void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public final String getBodyColor() {
+		return bodyColor;
+	}
+	public final void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+}
